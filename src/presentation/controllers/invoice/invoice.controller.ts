c35e8e7fb82df5bf2d4971093f88959ba2df3133
Renzo Reccio <reccio_2001@hotmail.com>
2024-07-26 21:13:25 -0500

    add response for invoice

--- src/presentation/controllers/invoice/invoice.controller.ts
+++ src/presentation/controllers/invoice/invoice.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreateInvoiceDto } from './create-invoice.dto';
 import { GenerateInvoiceCommand } from 'src/application/invoice/command/generate-invoice/generate-invoice.command';
+import { CustomResponse } from '../response/response.model';
 
 @Controller('invoice')
 export class InvoiceController {
@@ -13,8 +14,11 @@ export class InvoiceController {
 
     @Post()
     async generateInvoice(@Body() invoiceDto: CreateInvoiceDto): Promise<any> {
-
-        return await this._commandBus.execute(new GenerateInvoiceCommand(invoiceDto.month, invoiceDto.year));
+        return new CustomResponse<any>(
+            `Generated invoice for ${invoiceDto.month}-${invoiceDto.year}`,
+            await this._commandBus.execute(new GenerateInvoiceCommand(invoiceDto.month, invoiceDto.year)),
+            null
+        )
     }
 
     @Get()
